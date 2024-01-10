45e1ef5edf997ee280333db7eede2a98e5590e1c
Marcell Tarjan <marcell.tarjan@intel.com>
2024-01-10 01:05:54 +0000

    Added crc32

--- xbin/lcc-make.bat
+++ xbin/lcc-make.bat
@@ -6,7 +6,7 @@ set SRC_FILE=%1.c
 set EXE_FILE=%1.exe
 rem set LIBS=%LCCROOT%\lib64\sqlite3.lib
 
-set CFLAGS=-I"%LCCROOT%\include64" -O -A -unused
+set CFLAGS=-I"%LCCROOT%\include64" -O -A -unused %2 %3 %4 %5 %6 %7 %8
 set CC=%LCCROOT%\bin\lcc64.exe
 set LINKER=%LCCROOT%\bin\lcclnk64.exe
 set OBJ=%SRC_FILE:~0,-1%obj
